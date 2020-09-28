e5256a9514803823c861d08b38b546eb02a9afad
nilslambertz <nilslambertz@web.de>
2020-09-28 03:36:41 +0200

    added component for settings-bar

---
+++ src/Components/Settings/SettingsBar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './SettingsBar.css';
+
+function SettingsBar({size, solved, animationRunning, animationSpeed, changeSize, newMazeClick, animationClick, changeSpeed}) {
+    const playStyle = {
+        color: "lightgreen"
+    }
+    const stopStyle = {
+        color: "orange"
+    }
+
+    const newMazeStyle = {
+        color: "lightblue"
+    }
+
+    return (
+        <div id="settingsBar">
+            <div id="settingsDiv">
+                {/*<table className={(animationRunning ? "disabledSetting" : "")}>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <input type="range" min={50} max={200} value={size} step={10} onChange={changeSize}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            size
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>*/}
+                <table>
+                    <tbody>
+                    <tr>
+                        <td className={"settingsButton" + (solved ? " noClickSetting" : "")} rowSpan="2" style={animationRunning ? stopStyle : playStyle} onClick={animationClick}>
+                            {animationRunning ? "stop" : "start"}
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table className={(animationRunning ? "disabledSetting" : "")}>
+                    <tbody>
+                    <tr>
+                        <td className="settingsButton" rowSpan="2" style={newMazeStyle} onClick={newMazeClick}>
+                            new maze
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table className={(animationRunning ? "disabledSetting" : "")}>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <input type="range" min="10" max="500" value={animationSpeed} onChange={changeSpeed}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            animation speed
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default SettingsBar;
