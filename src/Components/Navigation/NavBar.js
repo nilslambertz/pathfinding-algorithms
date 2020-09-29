b712e9e420f0c18b0f13d429a68469bf906c2298
nilslambertz <nilslambertz@web.de>
2020-09-29 22:59:07 +0200

    changed nav-elem to recursive (not working yet)

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -5,8 +5,7 @@ import NavElem from './NavElem';
 function NavBar({algorithm, setAlgorithm, animationRunning}) {
     return (
         <div id="navbar">
-            <NavElem title="A*" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            <NavElem title="test" algoNumber="1" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
+            <NavElem title="Recursive" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
         </div>
     );
 }
