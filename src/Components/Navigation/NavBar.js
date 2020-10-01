3a57d58fcb147697538a052d4dd761bf874417e2
nilslambertz <nilslambertz@web.de>
2020-10-01 02:39:22 +0200

    added dijkstra to navigation-bar

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -5,7 +5,8 @@ import NavElem from './NavElem';
 function NavBar({algorithm, setAlgorithm, animationRunning}) {
     return (
         <div id="navbar">
-            <NavElem title="Recursive" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
+            <NavElem title="recursive" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
+            <NavElem title="dijkstra" algoNumber="1" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
         </div>
     );
 }
