a4fbd523fbc8d5adb5df6251d6eecba3deff81de
nilslambertz <nilslambertz@web.de>
2020-10-04 02:49:42 +0200

    added tremaux to navbar

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -5,8 +5,9 @@ import NavElem from './NavElem';
 function NavBar({algorithm, setAlgorithm, animationRunning}) {
     return (
         <div id="navbar">
-            <NavElem title="recursive" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
+            <NavElem title="basic recursion" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="dijkstra" algoNumber="1" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
+            <NavElem title="trémaux" algoNumber="2" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
         </div>
     );
 }
