890bf356da69ac3b1801dcc5149902943dd1a1c8
nilslambertz <nilslambertz@web.de>
2021-01-02 04:01:43 +0100

    added aStar to navigation-bar

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -5,6 +5,7 @@ import NavElem from './NavElem';
 function NavBar({algorithm, setAlgorithm, animationRunning}) {
     return (
         <div id="navbar">
+            <NavElem title="a*" algoNumber="4" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="dfs" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="dijkstra" algoNumber="1" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="greedy" algoNumber="3" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
