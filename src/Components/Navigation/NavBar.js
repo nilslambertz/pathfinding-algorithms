dd0b6834e1f4fc5dd9d1c46503d626d5423a2078
nilslambertz <nilslambertz@web.de>
2021-01-02 02:46:43 +0100

    reordered navbar to be in alphabetical order

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -7,8 +7,8 @@ function NavBar({algorithm, setAlgorithm, animationRunning}) {
         <div id="navbar">
             <NavElem title="dfs" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             {/*<NavElem title="dijkstra" algoNumber="1" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>*/}
-            <NavElem title="trémaux" algoNumber="2" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="greedy" algoNumber="3" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
+            <NavElem title="trémaux" algoNumber="2" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
         </div>
     );
 }
