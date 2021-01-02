4f6a27950f22f66518136c4e4757421abeead9e4
nilslambertz <nilslambertz@web.de>
2021-01-02 03:28:42 +0100

    removed old printDijkstra-function because it's not used anymore, dijsktra is printed with default function

--- src/Components/Content/Main.js
+++ src/Components/Content/Main.js
@@ -38,7 +38,7 @@ class Main extends React.Component {
                 return this.printDfsMaze();
             }
             case 1: {
-                return this.printDijkstraMaze();
+                return this.printDfsMaze();
             }
             case 2: {
                 return this.printTremauxMaze();
@@ -47,30 +47,6 @@ class Main extends React.Component {
                 return this.printDfsMaze();
             }
         }
-    }
-
-    printDijkstraMaze() {
-        return this.props.maze.map(function(c, i, a) {
-            return c.map(function(c2, i2, a2) {
-                if(c2 === 0) {
-                    return <div className={"mazeElem empty"} key={i+i2}/>;
-                } else if(c2 === 1) {
-                    return <div className={"mazeElem wall"} key={i+i2}/>;
-                } else if(c2 === 2) {
-                    return <div className={"mazeElem start"} key={i+i2}/>;
-                } else if(c2 === 3) {
-                    return <div className={"mazeElem end"} key={i+i2}/>
-                } else if(c2 === 4) {
-                    return <div className={"mazeElem correctPath"} key={i+i2}/>;
-                } else {
-                    let val = c2;
-                    let style = {
-                        backgroundColor: "rgb(" + val[0] + "," + val[1] + "," + val[2] + ")"
-                    }
-                    return <div className={"mazeElem"} style={style} key={i+i2}/>;
-                }
-            })
-        });
     }
 
     printDfsMaze() {
