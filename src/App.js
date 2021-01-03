1a980bf384a35842eff5e305c6cdc71b1ed459e0
nilslambertz <nilslambertz@web.de>
2021-01-03 02:02:56 +0100

    commenting code

--- src/App.js
+++ src/App.js
@@ -10,97 +10,116 @@ let animation;
 
 class App extends React.Component {
     state = {
-        algorithm: 2,
-        animationRunning: false,
-        solved: false,
-        maze: [],
-        saveMaze: [],
-        start: [],
-        end: [],
-        stepCount: 0,
+        algorithm: 2, // chosen algorithm, default tremaux
+        animationRunning: false, // if the animation is currently running
+        solved: false, // if the maze is solved
+        maze: [], // maze-array
+        saveMaze: [], // saved inital maze to reset animation
+        start: [], // starting point, array has two coordinates as values
+        end: [], // destination, array has two coordinates as values
+        stepCount: 0, // number of steps the algorithm has done
         /*width: 11,
         height: 7,/**/
         /**/width: 151,
         height: 75,/**/
-        animationSpeed: 500
+        animationSpeed: 500 // speed of the animation, higher values mean faster animation
     }
 
+    // Sets the algorithm
     setAlgorithm = (nr) => {
         nr = parseInt(nr);
-        if(nr === this.state.algorithm) return;
+        if(nr === this.state.algorithm) return; // If current algorithm was chosen, do nothing
+
+        // Reset stepCount and set algorithm to new number
         this.setState({stepCount: 0});
         this.setState({algorithm: nr}, () => {
-            let maze = [];
+            let maze = []; // Create new maze
             let saveMaze = this.state.saveMaze;
+
+            // Copy saveMaze into maze
             for(let i = 0; i < saveMaze.length; i++) {
                 maze[i] = [];
                 for (let j = 0; j < saveMaze[i].length; j++) {
                     maze[i][j] = saveMaze[i][j];
                 }
             }
+
+            // Change algorithm-number in Animation
             animation.changeAlgorithm(nr, maze);
+
+            // Set new maze and set solved to false
             this.setState({maze: maze, solved: false});
         });
     }
 
+    // Sets starting point
     setStart = (arr) => {
         this.setState({start: arr});
     }
 
+    // Sets destination
     setEnd = (arr) => {
         this.setState({end: arr});
     }
 
+    // Changes speed of the animation
     changeSpeed = (e) => {
-        if(this.state.animationRunning) return;
+        if(this.state.animationRunning) return; // Return if animation is currently running
+
+        // Change speed in state and Animation
         let newSpeed = parseFloat(e.target.value);
         this.setState({animationSpeed: newSpeed});
         animation.changeSpeed(newSpeed);
     }
 
+    // Creates new maze
     createMaze = () => {
-        let values = createMaze(this.state.height, this.state.width);
-        let maze = [];
-        for(let i = 0; i < this.state.height; i++) {
-            maze[i] = [];
-            for(let j = 0; j < this.state.width; j++) {
-                maze[i][j] = 1;
-            }
-        }
+        let values = createMaze(this.state.height, this.state.width); // Calls createMaze function in Functions.js
 
+        // Sets maze, starting point and destination
         this.setState({
             maze: values.maze,
             start: values.start,
             end: values.end,
             solved: false
         }, () => {
+            // Sends new values to Animation
             animation.changeMaze(values.maze.slice(0), values.steps, values.start, values.end);
         });
     }
 
+    // Creates new maze when component has mounted
     componentDidMount(){
         animation = new Animation(this.changeState, this.state.algorithm, this.addToStepCount);
         this.createMaze();
     }
 
+    // ChangeState-function to allow Animation to update the maze
     changeState = (e) => {
         this.setState(e);
     }
 
+    // Click-handler for start/stop animation button
     animationClick = () => {
         if(this.state.animationRunning) {
+            // If currently running, end animation
             animation.endAnimation();
         } else if(this.state.solved === false) {
+            // If not running and not solved, start animation
             animation.startAnimation();
         }
     }
 
+    // Click-handler for new-maze button
     newMazeClick = () => {
-        if(this.state.animationRunning) return;
+        if(this.state.animationRunning) return; // If animation running, do nothing
+
+        // Set stepCount to 0 and create new maze
         this.setState({stepCount: 0});
         this.createMaze();
     }
 
+    // Increases step count
     addToStepCount = (a) => {
         this.setState({stepCount: this.state.stepCount + a});
     }
