377f0fa6ce6302edde8f41af85ad35c7f4780d44
nilslambertz <nilslambertz@web.de>
2021-01-05 21:42:46 +0100

    commented and removed unused code

--- src/Algorithms/AStar.js
+++ src/Algorithms/AStar.js
@@ -1,42 +1,50 @@
 import { getPathNeighbours } from "../Utils/Functions";
 
-let PriorityQueue = require('priorityqueuejs');
+let PriorityQueue = require('priorityqueuejs'); // Using PriorityQueue for A*
 
 let steps;
 let maze;
 let wasHere;
 let queue;
 let path;
-let start;
 let end;
 
+// Get heuristic
 function getDistance(x, y) {
     return Math.abs(x - end[0]) + Math.abs(y - end[1]);
 }
 
+// A*-algorithm
 function aStar() {
+    // While PriorityQueue has more elements
     while(!queue.isEmpty()) {
-        let elem = queue.deq();
+        let elem = queue.deq(); // Get "best" element from queue
+        if(wasHere[elem.x][elem.y] === true) continue; // Continue, if already visisted
 
-        if(wasHere[elem.x][elem.y] === true) continue;
-        wasHere[elem.x][elem.y] = true;
-        steps.push(elem);
-
+        // If at destination
         if(elem.x === end[0] && elem.y === end[1]) {
+            // Iterate over parents and push nodes to path
             do {
                 path.push(elem);
             } while((elem = elem.parent) !== null);
             return;
         }
 
-        let neighbours = getPathNeighbours(maze, elem.x, elem.y);
-        for(let n of neighbours) {
-            if(wasHere[n.x][n.y] === true) continue;
+        // Set visited
+        wasHere[elem.x][elem.y] = true;
+        steps.push(elem);
 
-            n.distance = elem.distance + 1;
-            n.heuristic = getDistance(n.x, n.y);
-            n.parent = elem;
-            queue.enq(n);
+        // Get all neighbours
+        let neighbours = getPathNeighbours(maze, elem.x, elem.y);
+
+        // For every neighbours
+        for(let n of neighbours) {
+            if(wasHere[n.x][n.y] === true) continue; // Continue, if already visited
+
+            n.distance = elem.distance + 1; // Increase distance (depth of iteration)
+            n.heuristic = getDistance(n.x, n.y); // Set heuristic
+            n.parent = elem; // Set current node as parent
+            queue.enq(n); // Enqueue them
         }
 
     }
@@ -46,7 +54,6 @@ function getAStar(m, s, e) {
     maze = m;
     steps = [];
     end = e;
-    start = s;
     wasHere = [];
     path = [];
     for (let i = 0; i < m.length; i++) {
@@ -56,20 +63,24 @@ function getAStar(m, s, e) {
         }
     }
 
+    // Initialize PriorityQueue
     queue = new PriorityQueue(function(a, b) {
+        // Best distance + heuristic is at the front
         return (b.distance + b.heuristic) - (a.distance + a.heuristic);
     });
 
+    // Enqueue first element (starting node)
     let h = getDistance(s[0], s[1]);
     queue.enq({x: s[0], y: s[1], distance: 0, heuristic: h, parent: null});
 
-    aStar();
+    aStar(); // Start search
 
+    // Remove starting- and desination-node from path and steps
     steps.shift();
-    steps.pop();
     path.shift();
     path.pop();
 
+    // Return values
     return {
         steps: steps,
         path: path
