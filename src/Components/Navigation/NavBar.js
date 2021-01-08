f644f7e2dcc5d4ae46b40c6a5cd290c57d9238e7
nilslambertz <nilslambertz@web.de>
2021-01-08 20:53:19 +0100

    added tremaux back to navigation bar for testing

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -9,7 +9,7 @@ function NavBar({algorithm, setAlgorithm, animationRunning}) {
             <NavElem title="dfs" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="dijkstra" algoNumber="1" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="greedy" algoNumber="3" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            {/*<NavElem title="trémaux" algoNumber="2" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>*/}
+            <NavElem title="trémaux" algoNumber="2" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
         </div>
     );
 }
