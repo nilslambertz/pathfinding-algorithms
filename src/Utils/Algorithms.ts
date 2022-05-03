6e08136364a9b5c4c7be5e3895ad39133362125d
nilslambertz <nilslambertz@web.de>
2022-05-04 01:22:08 +0200

    add algorithms

---
+++ src/Utils/Algorithms.ts
@@ -0,0 +1,9 @@
+import { Algorithms, PathfindingAlgorithms } from "./Types";
+
+export const algorithms: PathfindingAlgorithms = {
+  [Algorithms.aStar]: () => [],
+  [Algorithms.dfs]: () => [],
+  [Algorithms.dijkstra]: () => [],
+  [Algorithms.greedy]: () => [],
+  [Algorithms.tremaux]: () => [],
+};
