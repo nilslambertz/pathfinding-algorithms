864a817935245c12082653975a9269626179693f
nilslambertz <nilslambertz@web.de>
2022-05-04 01:21:56 +0200

    add custom types

--- src/Utils/Types.ts
+++ src/Utils/Types.ts
@@ -1,3 +1,44 @@
 export interface PathElem {
   parent?: PathElem;
 }
+
+export interface StepDetails {
+  x: number;
+  y: number;
+  nextState: CellState;
+  // TODO
+}
+
+export type PathfindingAlgorithms = {
+  [key in Algorithms]: (maze: CellState[][]) => StepDetails[];
+};
+
+export enum Algorithms {
+  aStar = "a*",
+  dfs = "dfs",
+  dijkstra = "dijkstra",
+  greedy = "greedy",
+  tremaux = "tremaux",
+}
+
+export type BorderState = "red" | "orange";
+export interface CellStateWithBorder {
+  top?: BorderState;
+  right?: BorderState;
+  bottom?: BorderState;
+  left?: BorderState;
+}
+
+export enum CellStates {
+  Empty,
+  Wall,
+  Start,
+  End,
+}
+
+export type CellState =
+  | CellStates.Empty
+  | CellStates.Wall
+  | CellStates.Start
+  | CellStates.End
+  | CellStateWithBorder;
