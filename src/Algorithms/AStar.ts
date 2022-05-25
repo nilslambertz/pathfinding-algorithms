c84540d89f0ee71be90c89ff6a528b07e8250101
nilslambertz <nilslambertz@web.de>
2022-05-25 22:30:10 +0200

    convert A* to TS

---
+++ src/Algorithms/AStar.ts
@@ -0,0 +1,105 @@
+import { getPathNeighbours, getPathRecursive } from "../Utils/Functions";
+import PriorityQueue from "priorityqueuejs";
+import {
+  CellState,
+  CellStates,
+  PathElem,
+  StepDetails,
+  StepsAndCorrectPath,
+} from "../Utils/Types";
+
+interface PathElemWithHeuristic extends PathElem {
+  heuristic?: number;
+}
+
+// Get heuristic
+function getDistance(x: number, y: number, end: [number, number]) {
+  return Math.abs(x - end[0]) + Math.abs(y - end[1]);
+}
+
+interface AStarArgs {
+  maze: CellState[][];
+  start: [number, number];
+  end: [number, number];
+  steps: StepDetails[];
+  correctPath: [number, number][];
+}
+
+// A*-algorithm
+function aStar({ maze, start, end, steps, correctPath }: AStarArgs) {
+  const queue = new PriorityQueue(function (
+    a: PathElemWithHeuristic,
+    b: PathElemWithHeuristic
+  ) {
+    if (!b.distance || !a.distance || !b.heuristic || !a.heuristic) return 0;
+
+    // Best distance + heuristic is at the front
+    return b.distance + b.heuristic - (a.distance + a.heuristic);
+  });
+  const visited = maze.map((row) => Array(row.length).fill(false));
+
+  // Enqueue first element (starting node)
+  let h = getDistance(start[0], start[1], end);
+  queue.enq({ x: start[0], y: start[1], distance: 0, heuristic: h });
+
+  // While PriorityQueue has more elements
+  while (!queue.isEmpty()) {
+    let elem = queue.deq(); // Get "best" element from queue
+    if (visited[elem.x][elem.y] === true) continue; // Continue, if already visisted
+
+    // If at destination
+    if (elem.x === end[0] && elem.y === end[1]) {
+      correctPath = getPathRecursive(elem).map((coords) => [
+        coords.x,
+        coords.y,
+      ]);
+      return;
+    }
+
+    // Set visited
+    visited[elem.x][elem.y] = true;
+    steps.push({
+      nextState: CellStates.Visited,
+      cells: [[elem.x, elem.y]],
+    });
+
+    // Get all neighbours
+    let neighbours = getPathNeighbours(
+      maze,
+      elem.x,
+      elem.y
+    ) as PathElemWithHeuristic[];
+
+    // For every neighbours
+    for (let n of neighbours) {
+      if (visited[n.x][n.y] === true) continue; // Continue, if already visited
+
+      n.distance = (elem.distance ?? 0) + 1; // Increase distance (depth of iteration)
+      n.heuristic = getDistance(n.x, n.y, end); // Set heuristic
+      n.parent = elem; // Set current node as parent
+      queue.enq(n); // Enqueue them
+    }
+  }
+}
+
+export const getAStarSteps = (
+  maze: CellState[][],
+  start: [number, number],
+  end: [number, number]
+): StepsAndCorrectPath => {
+  const steps: StepDetails[] = [];
+  const correctPath: [number, number][] = [];
+
+  aStar({
+    maze,
+    start,
+    end,
+    steps,
+    correctPath,
+  });
+
+  return {
+    steps,
+    correctPath,
+  };
+};
--- src/Utils/Algorithms.ts
+++ src/Utils/Algorithms.ts
@@ -1,9 +1,10 @@
+import { getAStarSteps } from "../Algorithms/AStar";
 import { getDfsSteps } from "../Algorithms/Dfs";
 import { getGreedySteps } from "../Algorithms/GreedySearch";
 import { Algorithms, PathfindingAlgorithms } from "./Types";
 
 export const algorithms: PathfindingAlgorithms = {
-  [Algorithms.aStar]: () => ({}),
+  [Algorithms.aStar]: getAStarSteps,
   [Algorithms.dfs]: getDfsSteps,
   [Algorithms.dijkstra]: () => ({}),
   [Algorithms.greedy]: getGreedySteps,
