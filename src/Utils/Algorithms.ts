d183aded100bb025992a129ee2dc65c6b92c468a
nilslambertz <nilslambertz@web.de>
2022-06-06 02:46:33 +0200

    add Tremaux algorithm

--- src/Utils/Algorithms.ts
+++ src/Utils/Algorithms.ts
@@ -2,6 +2,7 @@ import { getAStarSteps } from "../Algorithms/AStar";
 import { getDfsSteps } from "../Algorithms/Dfs";
 import { getDijkstraSteps } from "../Algorithms/Dijkstra";
 import { getGreedySteps } from "../Algorithms/GreedySearch";
+import { getTremauxSteps } from "../Algorithms/Tremaux";
 import { Algorithms, PathfindingAlgorithms } from "./Types";
 
 export const algorithms: PathfindingAlgorithms = {
@@ -9,5 +10,5 @@ export const algorithms: PathfindingAlgorithms = {
   [Algorithms.dfs]: getDfsSteps,
   [Algorithms.dijkstra]: getDijkstraSteps,
   [Algorithms.greedy]: getGreedySteps,
-  //[Algorithms.tremaux]: () => ({}),
+  [Algorithms.tremaux]: getTremauxSteps,
 };
